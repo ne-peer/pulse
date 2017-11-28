0cdf46711908a7c4f0507f186392dd6de850c42d
ne-peer <mileage0110@gmail.com>
2017-11-28 17:08:45 +0900

    [update] chage site title

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'pulse-portal-site',
+    title: 'Pulse Portal',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },

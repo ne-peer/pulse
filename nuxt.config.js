fa787b7c87892ce693d7ff43af39003542d492d9
ne-peer <mileage0110@gmail.com>
2017-11-28 13:07:21 +0900

    [update] applied custom spectre scss

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,9 +14,10 @@ module.exports = {
     ]
   },
   css: [
-    'node_modules/spectre.css/dist/spectre.min.css',
-    'node_modules/spectre.css/dist/spectre-exp.min.css',
-    'node_modules/spectre.css/dist/spectre-icons.min.css'
+    { src: '~assets/css/pulse.scss', lang: 'scss' }
+    // 'node_modules/spectre.css/dist/spectre.min.css',
+    // 'node_modules/spectre.css/dist/spectre-exp.min.css',
+    // 'node_modules/spectre.css/dist/spectre-icons.min.css'
   ],
   /*
   ** Customize the progress bar color

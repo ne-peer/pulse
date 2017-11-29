0003a0c236996cc285815ea812cde5775fdd0752
ne-peer <mileage0110@gmail.com>
2017-11-29 18:18:47 +0900

    [update] Profile style

--- components/Profile.vue
+++ components/Profile.vue
@@ -1,17 +1,25 @@
 <template>
   <section>
-    <div class="tile">
-      <div class="tile-icon">
-        <div class="example-tile-icon">
-          <i class="icon icon-file centered"></i>
-        </div>
-      </div>
-      <div class="tile-content">
-        <p class="tile-title">The Avengers</p>
-        <p class="tile-subtitle text-gray">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium voluptatibus fuga consequuntur blanditiis ut delectus. Nisi nemo temporibus nesciunt cupiditate?
-        </p>
-      </div>
+    <div class="avater">
+      <img src="~/static/avater/20170306_dia-icon.png" height="150" width="150" class="img-fit-contain circle" alt="my-avater">
+    </div>
+    <div class="profile">
+      <p><h3>The Avengers</h3></p>
+      <p class="text-gray">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium voluptatibus fuga consequuntur blanditiis ut delectus. Nisi nemo temporibus nesciunt cupiditate?
+      </p>
     </div>
   </section>
 </template>
+
+<style>
+.avater {
+  width: 150px;
+  margin: 0 auto;
+}
+
+.profile {
+  margin-top: 10px;
+  padding: 20px;
+}
+</style>

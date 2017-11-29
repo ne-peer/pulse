113161c2ad812b99ae580a99bc8271d25aa64185
ne-peer <mileage0110@gmail.com>
2017-11-29 17:56:45 +0900

    [update] fix career

--- components/Career.vue
+++ components/Career.vue
@@ -3,38 +3,37 @@
     <div class="columns">
       <div class="column col-10 col-mx-auto">
         <div class="timeline">
-          <div class="timeline-item" id="timeline-example-1">
+          <div class="timeline-item">
             <div class="timeline-left">
-              <a class="timeline-icon" href="#timeline-example-1"></a>
+              <a class="timeline-icon icon-lg">
+                <i class="icon icon-check"></i>
+              </a>
             </div>
             <div class="timeline-content">
+              <p><b>now</b></p>
               <p>Lorem ipsum dolor sit amet consectetur.</p>
               <p>Eveniet facilis hic blanditiis porro labore?</p>
             </div>
           </div>
         
-          <div class="timeline-item" id="timeline-example-2">
+          <div class="timeline-item">
             <div class="timeline-left">
-              <a class="timeline-icon icon-lg" href="#timeline-example-2">
-                <i class="icon icon-check"></i>
-              </a>
+              <a class="timeline-icon"></a>
             </div>
             <div class="timeline-content">
-              <p>Lorem ipsum dolor sit amet.</p>
+              <p><b>Lorem ipsum dolor sit amet.</b></p>
               <p>Impedit fuga ex eveniet dolores.</p>
               <p>Amet ex voluptatibus corrupti vero!</p>
               <p>Numquam, corrupti. Blanditiis, praesentium ex.</p>
             </div>
           </div>
         
-          <div class="timeline-item" id="timeline-example-2">
+          <div class="timeline-item">
             <div class="timeline-left">
-              <a class="timeline-icon icon-lg" href="#timeline-example-2">
-                <i class="icon icon-check"></i>
-              </a>
+              <a class="timeline-icon"></a>
             </div>
             <div class="timeline-content">
-              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <p><b>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</b></p>
               <p>Aperiam libero maxime magni ipsa tenetur odit eius.</p>
               <p>Sed, officiis iure magnam quidem tempora magni quibusdam!</p>
             </div>
@@ -42,12 +41,11 @@
         
           <div class="timeline-item" id="timeline-example-2">
             <div class="timeline-left">
-              <a class="timeline-icon icon-lg" href="#timeline-example-2">
-                <i class="icon icon-check"></i>
-              </a>
+              <a class="timeline-icon"></a>
             </div>
             <div class="timeline-content">
-              <p>now</p>
+              <p><b>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, labore!</b></p>
+              <p>Odit necessitatibus excepturi dignissimos optio sit cum, autem ipsam quasi.</p>
             </div>
           </div>
         </div>

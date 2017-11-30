5e043656c57b2ab3d5759058eacb4e668ba5ad6c
ne-peer <mileage0110@gmail.com>
2017-11-30 16:30:36 +0900

    [update] 経歴を削除

--- components/Skill.vue
+++ components/Skill.vue
@@ -55,31 +55,7 @@
       </div>
       <div class="panel-footer"></div>
     </div>
-    
-    <div class="panel">
-      <div class="panel-header">
-        <div class="panel-title">実務経験</div>
-      </div>
-      <div class="panel-body description-indent">
-        <div class="tile">
-          <div class="tile-content">
-            <p class="tile-title" lang="ja"><h4>Eコマースシステムの開発</h4></p>
-            <p class="tile-subtitle text-gray" lang="ja">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut reiciendis iste consectetur quos dicta officia aperiam? Aliquid dolorum ex quasi nobis perspiciatis commodi facilis voluptas iste quibusdam inventore. Repudiandae magnam explicabo consectetur cum eligendi cupiditate perspiciatis odit dolore qui harum, dolorum atque adipisci quis obcaecati? Et earum, amet, quia vitae ratione corporis tenetur sequi eum, error at perferendis vel officia laboriosam dignissimos autem iusto fugiat fugit nam saepe placeat exercitationem nisi. Magnam, minima omnis nihil molestias laudantium deserunt, delectus alias quae ut repellat ipsum? Quam maiores maxime quas, magni fuga assumenda! Consequatur accusantium corrupti voluptatem labore doloribus officia autem omnis.
-            </p>
-          </div>
-        </div>
-        <div class="tile">
-          <div class="tile-content">
-            <p class="tile-title" lang="ja"><h4>Webサイトの構築</h4></p>
-            <p class="tile-subtitle text-gray" lang="ja">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident laborum aut nulla quibusdam consequuntur, doloremque quisquam, natus veritatis omnis velit incidunt tenetur, id accusamus esse laudantium eaque totam suscipit repellat temporibus quidem cupiditate tempore cumque! Fuga recusandae voluptatibus ipsa quisquam reiciendis voluptates cum blanditiis illum in voluptas, sequi ipsum similique voluptatum numquam magni cupiditate et nulla cumque molestias libero excepturi aliquam! Voluptatem asperiores dolor maxime neque animi beatae odit magni!
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="panel-footer"></div>
-    </div>
+
   </section>
 </template>
 
@@ -89,22 +65,24 @@ section.skill-section {
 }
 
 div.panel {
-    margin-bottom: 30px;
+  margin-bottom: 30px;
+  /* background-color: #727e96;
+  color: #f8f9fa; */
 }
 
 .description-indent {
-    margin-left: 20px;
+  margin-left: 20px;
 }
 
 p.tile-subtitle {
-    margin-left: 10px;
+  margin-left: 10px;
 }
 
 div.tile-icon {
-    margin: 20px;
+  margin: 20px;
 }
 
 div.tile-content {
-    margin-left: 10px;
+  margin-left: 10px;
 }
 </style>

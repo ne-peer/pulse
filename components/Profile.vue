9449d04019207181127681c11184a1e255a33544
ne-peer <mileage0110@gmail.com>
2017-12-01 18:18:46 +0900

    [update] profile margin

--- components/Profile.vue
+++ components/Profile.vue
@@ -1,9 +1,9 @@
 <template>
-  <section>
+  <section class="profile">
     <div class="avater">
       <img src="~/static/avater/20170306_dia-icon.png" height="150" width="150" class="img-fit-contain circle" alt="my-avater">
     </div>
-    <div class="profile">
+    <div class="detail">
       <p><h3>The Avengers</h3></p>
       <p class="text-gray">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium voluptatibus fuga consequuntur blanditiis ut delectus. Nisi nemo temporibus nesciunt cupiditate?
@@ -13,13 +13,14 @@
 </template>
 
 <style>
-.avater {
+.profile .avater {
   width: 150px;
   margin: 0 auto;
 }
 
-.profile {
-  margin-top: 10px;
-  padding: 20px;
+.profile .detail {
+  margin-top: 30px;
+  padding-left: 60px;
+  padding-right: 60px;
 }
 </style>

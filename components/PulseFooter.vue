6d5ba2a53ba4fe673e9ea04ff04ed36458e2d5fc
ne-peer <mileage0110@gmail.com>
2017-12-28 15:55:38 +0900

    [update] footer style fix

--- components/PulseFooter.vue
+++ components/PulseFooter.vue
@@ -1,17 +1,17 @@
 <template>
   <footer>
-    <div class="author">
-      <div class="name">Pulse</div>
-      <address class="mail">
-        <i class="fa fa-envelope-o" aria-hidden="true"></i> mileage0110@gmail.com
-      </address>
-    </div>
     <div class="attribution">
       <a href="https://www.contentful.com/" rel="nofollow" target="_blank">
         <img src="https://images.contentful.com/fo9twyrwpveg/7F5pMEOhJ6Y2WukCa2cYws/398e290725ef2d3b3f0f5a73ae8401d6/PoweredByContentful_DarkBackground.svg"
              style="max-width:100px;width:100%;"
              alt="Powered by Contentful" />
       </a>
+    </div>
+    <div class="author">
+      <div class="name">Pulse</div>
+      <address class="mail">
+        <i class="fa fa-envelope-o" aria-hidden="true"></i> mileage0110@gmail.com
+      </address>
     </div>
   </footer>
 </template>
@@ -20,11 +20,11 @@
 footer {
   margin-top: 40px;
   background-color: #454d5d;
-  height: 210px;
+  height: 200px;
 }
 
 footer .author {
-  padding-top: 44px;
+  padding-top: 12px;
   width: 200px;
   margin: 0 auto;
   text-align: center;
@@ -45,8 +45,8 @@ footer .mail {
 }
 
 footer .attribution {
-  margin-top: 68px;
-  margin-right: 20px;
+  padding-top: 12px;
+  margin-right: 16px;
   text-align: right;
 }
 </style>

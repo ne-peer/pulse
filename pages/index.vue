82b0330c4f3c6a3890c30dfd4b0a005c6d99315b
ne-peer <mileage0110@gmail.com>
2018-01-18 18:02:58 +0900

    [update] top page

--- pages/index.vue
+++ pages/index.vue
@@ -1,51 +1,31 @@
 <template>
   <section class="index">
     <div>
-      <div class="logo">
-        <logo/>
-      </div>
-      <div class="main container">
-        <div class="columns">
-          <div class="column col-8 col-mx-auto">
-            <p class="title">Pulse</p>
-            <p class="subtitle">Portal site of Pulse.</p>
-          </div>
-        </div>
-      </div>
+      <img src="~/static/symfony-ghoast.png" width="100" class="img-responsive" alt="ghoast">
+      <p class="ja text-gray">ここには何もありません。</p>
+      <p class="en text-gray">There is nothing here.</p>
     </div>
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
 </script>
 
 <style>
-section.index .logo {
-  margin-bottom: 25px;
-  width: 200px;
+.index img {
   margin: 0 auto;
+  padding-top: 200px;
 }
 
-section.index .title {
-  display: block;
-  font-weight: 300;
-  font-size: 64px;
-  color: #35495e;
-  letter-spacing: 1px;
+.index p {
+  text-align: center;
 }
 
-section.index .subtitle {
-  font-weight: 300;
-  font-size: 24px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+.index .ja {
+  margin-top: 40px;
+}
+
+.index .en {
+  font-size: 10px;
 }
 </style>

7a770e5075ebbf843ffc4740a96156187860b9c5
ne-peer <mileage0110@gmail.com>
2018-01-18 18:36:59 +0900

    [fix] footer design

--- components/PulseFooter.vue
+++ components/PulseFooter.vue
@@ -1,17 +1,18 @@
 <template>
   <footer>
-    <div class="attribution">
-      <a href="https://www.contentful.com/" rel="nofollow" target="_blank">
-        <img src="https://images.contentful.com/fo9twyrwpveg/7F5pMEOhJ6Y2WukCa2cYws/398e290725ef2d3b3f0f5a73ae8401d6/PoweredByContentful_DarkBackground.svg"
-             style="max-width:100px;width:100%;"
-             alt="Powered by Contentful" />
-      </a>
-    </div>
     <div class="author">
       <div class="name">Pulse</div>
       <address class="mail">
         <i class="fa fa-envelope-o" aria-hidden="true"></i> mileage0110@gmail.com
       </address>
+    </div>
+
+    <div class="spacer">
+      <div class="divider"></div>
+    </div>
+
+    <div class="attribution">
+      <a href="https://www.contentful.com/" rel="nofollow" target="_blank"><img src="https://images.contentful.com/fo9twyrwpveg/7Htleo27dKYua8gio8UEUy/0797152a2d2f8e41db49ecbf1ccffdaa/PoweredByContentful_DarkBackground_MonochromeLogo.svg" style="max-width:100px;width:100%;" alt="Powered by Contentful" /></a>
     </div>
   </footer>
 </template>
@@ -20,18 +21,18 @@
 footer {
   margin-top: 40px;
   background-color: #454d5d;
-  height: 200px;
+  height: 220px;
 }
 
 footer .author {
-  padding-top: 12px;
+  padding-top: 50px;
   width: 200px;
   margin: 0 auto;
   text-align: center;
 }
 
 footer .name {
-  color: #ffffff;
+  color: #5adedc;
   font-weight: 300;
   font-size: 22px;
   letter-spacing: 10px;
@@ -45,9 +46,20 @@ footer .mail {
   padding-right: 8px;
 }
 
+footer .spacer {
+  margin-top: 10px;
+  margin-bottom: 40px;
+  padding-right: 16px;
+}
+
+footer .divider {
+  margin: 0 auto;
+  width: 12px;
+  border: solid #999 1px;
+}
+
 footer .attribution {
-  padding-top: 12px;
   margin-right: 16px;
-  text-align: right;
+  text-align: center;
 }
 </style>

a00dd38ae5692ded8bb833005a2944145791bf2e
ne-peer <mileage0110@gmail.com>
2018-02-19 12:12:32 +0900

    [update] add parallax

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,53 +1,47 @@
 <template>
   <section class="blog">
-    <div class="page-title">
-      <h2>Blog</h2>
-      <div class="pulse-border"></div>
+    <div class="parallax">
+      <div class="parallax-content">
+        <div class="parallax-front">
+          <h2>Blog</h2>
+        </div>
+        <div class="parallax-back">
+          <img src="https://ne-peer.ams3.digitaloceanspaces.com/resource/bheader.JPG" class="img-responsive rounded" height="400">
+        </div>
+      </div>
     </div>
     
-    <paginate-links v-scroll-to="{ element: '.outer' }" @change="onLangsPageChange" for="blog" :show-step-links="true"
-      :step-links="{ next: 'Next', prev: 'Prev' }" :classes="{ 'ul': 'pagination', 'li': 'page-item', '.next > a': 'next-link', '.prev > a': 'prev-link' }"
-    ></paginate-links>
-    <paginate name="blog" :list="blog" :per="6" tag="div">
-      <div class="container">
-        <div class="timeline">
-          <div class="columns" v-for="post in paginated('blog')">
-            <div class="column col-2 col-lg-3 timeline-item hide-md">
-              <div class="timeline-left">
-                <a class="timeline-icon"></a>
-              </div>
-              <div class="timeline-content text-gray">
-                # {{ (new Date(post.fields.publishDate)).getFullYear() }}.{{ (new Date(post.fields.publishDate)).getMonth() + 1 }}.{{ (new Date(post.fields.publishDate)).getDate() }}
-              </div>
-            </div>
-            <nuxt-link class="column col-9 col-md-12" tag="div" :to="{ name: 'blog-slug', params: { slug: post.fields.slug }}">
-              <div class="panel">
-                <div class="panel-header">
-                  <div class="publish-date show-md">
-                    {{ (new Date(post.fields.publishDate)).getFullYear() }}年 
-                    {{ (new Date(post.fields.publishDate)).getMonth() + 1 }}月 
-                    {{ (new Date(post.fields.publishDate)).getDate() }}日
-                  </div>
+    <div class="pulse-border"></div>
+    
+    <div class="contents">
+      <paginate-links v-scroll-to="{ element: '.outer' }" @change="onLangsPageChange" for="blog" :show-step-links="true"
+        :step-links="{ next: 'Next', prev: 'Prev' }" :classes="{ 'ul': 'pagination', 'li': 'page-item', '.next > a': 'next-link', '.prev > a': 'prev-link' }"
+      ></paginate-links>
+      <paginate name="blog" :list="blog" :per="6" tag="div">
+        <div class="container">
+          <div class="timeline">
+            <div class="columns" v-for="post in paginated('blog')">
+              <div class="column col-2 col-lg-3 timeline-item hide-md">
+                <div class="timeline-left">
+                  <a class="timeline-icon"></a>
                 </div>
-                <div class="panel-body">
-                  <div class="container">
-                    <div class="columns">
-                      <div class="column show-sm photo img-center singlecolumn-img">
-                        <img
-                          v-if="post.fields.heroImage"
-                          class="img-responsive"
-                          alt="heading photo"
-                          :src="post.fields.heroImage.fields.file.url">
-                      </div>
+                <div class="timeline-content text-gray">
+                  # {{ (new Date(post.fields.publishDate)).getFullYear() }}.{{ (new Date(post.fields.publishDate)).getMonth() + 1 }}.{{ (new Date(post.fields.publishDate)).getDate() }}
+                </div>
+              </div>
+              <nuxt-link class="column col-9 col-md-12" tag="div" :to="{ name: 'blog-slug', params: { slug: post.fields.slug }}">
+                <div class="panel">
+                  <div class="panel-header">
+                    <div class="publish-date show-md">
+                      {{ (new Date(post.fields.publishDate)).getFullYear() }}年 
+                      {{ (new Date(post.fields.publishDate)).getMonth() + 1 }}月 
+                      {{ (new Date(post.fields.publishDate)).getDate() }}日
                     </div>
-                    <template v-if="post.fields.heroImage">
-                      <!-- 見出し画像あり -->
+                  </div>
+                  <div class="panel-body">
+                    <div class="container">
                       <div class="columns">
-                        <div class="column col-18 col-sm-12 description">
-                          <h4>{{ post.fields.title }}</h4>
-                          <div class="description text-gray">{{ post.fields.description }}</div>
-                        </div>
-                        <div class="column col-4 hide-sm photo">
+                        <div class="column show-sm photo img-center singlecolumn-img">
                           <img
                             v-if="post.fields.heroImage"
                             class="img-responsive"
@@ -55,31 +49,47 @@
                             :src="post.fields.heroImage.fields.file.url">
                         </div>
                       </div>
-                    </template>
-                    <template v-else>
-                      <!-- 見出し画像なし -->
-                      <div class="columns">
-                        <div class="column col-12 description">
-                          <h4>{{ post.fields.title }}</h4>
-                          <div class="description text-gray">{{ post.fields.description }}</div>
+                      <template v-if="post.fields.heroImage">
+                        <!-- 見出し画像あり -->
+                        <div class="columns">
+                          <div class="column col-18 col-sm-12 description">
+                            <h4>{{ post.fields.title }}</h4>
+                            <div class="description text-gray">{{ post.fields.description }}</div>
+                          </div>
+                          <div class="column col-4 hide-sm photo">
+                            <img
+                              v-if="post.fields.heroImage"
+                              class="img-responsive"
+                              alt="heading photo"
+                              :src="post.fields.heroImage.fields.file.url">
+                          </div>
                         </div>
-                      </div>
-                    </template>
+                      </template>
+                      <template v-else>
+                        <!-- 見出し画像なし -->
+                        <div class="columns">
+                          <div class="column col-12 description">
+                            <h4>{{ post.fields.title }}</h4>
+                            <div class="description text-gray">{{ post.fields.description }}</div>
+                          </div>
+                        </div>
+                      </template>
+                    </div>
+                  </div>
+                  <div class="panel-footer">
+                    <div class="action">Read More</div>
                   </div>
                 </div>
-                <div class="panel-footer">
-                  <div class="action">Read More</div>
-                </div>
-              </div>
-            </nuxt-link>
+              </nuxt-link>
+            </div>
           </div>
         </div>
-      </div>
-    </paginate>
-    
-    <paginate-links v-scroll-to="{ element: '.outer' }" @change="onLangsPageChange" for="blog" :show-step-links="true"
-      :step-links="{ next: 'Next', prev: 'Prev' }" :classes="{ 'ul': 'pagination', 'li': 'page-item', '.next > a': 'next-link', '.prev > a': 'prev-link' }"
-    ></paginate-links>
+      </paginate>
+      
+      <paginate-links v-scroll-to="{ element: '.outer' }" @change="onLangsPageChange" for="blog" :show-step-links="true"
+        :step-links="{ next: 'Next', prev: 'Prev' }" :classes="{ 'ul': 'pagination', 'li': 'page-item', '.next > a': 'next-link', '.prev > a': 'prev-link' }"
+      ></paginate-links>
+    </div>
   </section>
 </template>
 
@@ -142,15 +152,18 @@ export default {
   padding-bottom: 20px;
 }
 
-.page-title {
-  text-align: center;
-  margin-bottom: 10px;
+.contents {
+  margin-top: 20px;
 }
 
 .panel {
-  margin-bottom: 10px;
+  margin-bottom: 30px;
   padding: 2%;
   cursor: pointer;
+}
+
+.parallax {
+  max-height: 400px;
 }
 
 .panel-header {
